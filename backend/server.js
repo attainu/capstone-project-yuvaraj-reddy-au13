c563ee324a7aeb00844b9eb3465e5770c1623638
rohit <rohitbilung@gmail.com>
2021-06-17 01:34:24 +0530

    added .env file and connected to mongodb using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
 const app = express()
+const PORT = process.env.PORT || 5000
+
+dotenv.config()
 
 app.get('/', (req,res)=>{
-    res.send('working')
+    res.send('Apis running..')
 })
 
-app.listen(5000, console.log("server running at port 5000"))
+app.listen(PORT,()=>{
+    console.log("server started at ", PORT);
+})
